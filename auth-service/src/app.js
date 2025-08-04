4b1040065aa97cc286fad54d496c551933902192
Jose Abraham <jabrahamocampo@users.noreply.github.com>
2025-08-04 17:35:44 -0600

    Update app.js

--- auth-service/src/app.js
+++ auth-service/src/app.js
@@ -1,19 +1,15 @@
 import express from 'express';
-//import cors from 'cors';
 import morgan from 'morgan';
 import authRoutes from './routes/auth.routes.js';
 
 const app = express();
 
-// Middlewares
-//app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
-// Routes
-app.use('/', authRoutes);
+// Rutas con prefijo unificado
+app.use('/api/v1/auth', authRoutes);
 
-// Base Route for health check
 app.get('/', (req, res) => {
   res.send('Auth Service is running');
 });
