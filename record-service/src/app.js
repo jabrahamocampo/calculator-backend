c2276b3406c17084c1ef68cbffe486b23bb600c4
Jose Abraham <jabrahamocampo@users.noreply.github.com>
2025-08-04 16:30:43 -0600

    Update app.js

--- record-service/src/app.js
+++ record-service/src/app.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import cors from 'cors';
+//import cors from 'cors';
 import morgan from 'morgan';
 import recordRoutes from './routes/record.routes.js';
 
 const app = express();
 
 // Middlewares
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
