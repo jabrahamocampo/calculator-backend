0f90211419f83b1b619e81a32369321b71429e1c
Jose Abraham <jabrahamocampo@users.noreply.github.com>
2025-08-04 12:16:20 -0600

    Update database.js

--- balance-service/src/config/database.js
+++ balance-service/src/config/database.js
@@ -3,16 +3,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    logging: false, 
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false,
+});
 
 export async function syncDatabase() {
   if (process.env.NODE_ENV === 'test') {
