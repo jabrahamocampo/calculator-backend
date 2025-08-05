eec8aef317168034f93855ec4490d57a239924c5
Jose Abraham <jabrahamocampo@users.noreply.github.com>
2025-08-04 23:18:05 -0600

    Update index.js

--- api-gateway/src/index.js
+++ api-gateway/src/index.js
@@ -12,12 +12,7 @@ const allowedOrigins = [
 ];
 
 app.use((req, res, next) => {
-  console.log("📥 Llega al API Gateway:", req.method, req.url);
-  next();
-});
-
-app.use((req, res, next) => {
-   console.log("📥 Llega al API Gateway:", req.method, req.url, req.headers.origin);
+   console.log("📥 Llega al API Gateway:", req.method, req.url);
   
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
