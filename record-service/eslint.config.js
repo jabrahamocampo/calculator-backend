687ada9e89d7ad1ac9c4546d81e5eec1728957b9
Jose Abraham <jabrahamocampo@users.noreply.github.com>
2025-09-09 11:00:17 -0600

    Update eslint.config.js

--- record-service/eslint.config.js
+++ record-service/eslint.config.js
@@ -7,6 +7,25 @@ export default [
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
+      globals: {
+        // Node.js globals
+        process: "readonly",
+        console: "readonly",
+        __dirname: "readonly",
+        __filename: "readonly",
+        exports: "readonly",
+        module: "readonly",
+        require: "readonly",
+        Buffer: "readonly",
+        setImmediate: "readonly",
+        clearImmediate: "readonly",
+        setInterval: "readonly",
+        clearInterval: "readonly",
+        setTimeout: "readonly",
+        clearTimeout: "readonly",
+        global: "readonly",
+        queueMicrotask: "readonly",
+      },
     },
     rules: {
       "no-unused-vars": "warn",
